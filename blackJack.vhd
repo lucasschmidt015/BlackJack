0919e342d7549c5cff73016c1a6277f69e05b61e
Lucas Schmidt <lucas.schmidt015@gmail.com>
2024-11-14 19:29:51 -0300

    Adicionado arquivo blackjack.vhd

---
+++ blackJack.vhd
@@ -0,0 +1,16 @@
+library ieee;
+use ieee.std_logic_1164.all;
+
+entity blackJack is 
+    port(
+        
+    );
+end blackJack;
+
+architecture behaviour of blackJack is 
+begin
+    process()
+        
+
+    end process;
+end behaviour;
