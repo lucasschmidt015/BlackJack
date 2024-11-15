a8f5dfe57da1c5d00a43fe31457513b6826c13c7
Lucas Schmidt <lucas.schmidt015@gmail.com>
2024-11-15 16:27:36 -0300

    Criado lógica do start.

--- blackJack.vhd
+++ blackJack.vhd
@@ -19,15 +19,23 @@ architecture behaviour of blackJack is
         );
     end component;
 begin
-    process(clk, start) -- Lembrar de lidar com o caso onde o usuário começa a dar clock sem ter dado start
+    process(clock, start) -- Lembrar de lidar com o caso onde o usuário começa a dar clock sem ter dado start
     signal playerSUM, DealerSUM: integer range 0 to 100 := 0; -- Precisa de um range pra FPGA entender
     signal clockCount: integer range 0 to 100;
+    signal gameStarted: sdt_logic := '0';
     variable pickedCard: integer range 0 to 100;
     begin
         if (start = '0') then 
             -- Reseta tudo
+            gameStarted <= '1';
+            pickedCard := 0;
             clockCount <= 0;
-        else
+            playerSUM <= 0;
+            DealerSUM <= 0;
+            win <= '0';
+            tie <= '0';
+            lose <= '0';
+        elsif (gameStarted = '1') then
             if ((clockCount = 0 OR clockCount = 1) AND clock = '0') then
                 clockCount <= clockCount + 1;  -- Lembrar que o valor só é atualizado no próximo ciclo
 
@@ -42,22 +50,13 @@ begin
             end if;
 
             if (playerSUM = 21) then
+
                 win <= '1';
                 tie <= '0';
                 lose <= '0';
                 
                 sun <= std_logic_vector(to_unsigned(playerSUM, sun'length)); -- Precisa converter para std_logic_vector
             end if;
-            
-
-
-            -- Compra duas cartas
-
-            -- Vai para o espera 
-
-
-
-
 
         end if;
     end process;
