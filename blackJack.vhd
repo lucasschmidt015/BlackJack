9d7b1fd4171d612460a7be01e42611b897e3d659
Lucas Schmidt <lucas.schmidt015@gmail.com>
2024-11-15 10:57:59 -0300

    Iniciado a lógica da compra de cartas do player.

--- blackJack.vhd
+++ blackJack.vhd
@@ -12,14 +12,42 @@ entity blackJack is
 end blackJack;
 
 architecture behaviour of blackJack is 
+    component randomGenerator is
+        port(
+            clk: in std_logic;
+            stim: out integer
+        );
+    end component;
 begin
     process(clk, start) -- Lembrar de lidar com o caso onde o usuário começa a dar clock sem ter dado start
-    signal playerSUM, DealerSUM: integer range 0 to 100; -- Precisa de um range pra FPGA entender
+    signal playerSUM, DealerSUM: integer range 0 to 100 := 0; -- Precisa de um range pra FPGA entender
+    signal clockCount: integer range 0 to 100;
+    variable pickedCard: integer range 0 to 100;
     begin
         if (start = '0') then 
             -- Reseta tudo
+            clockCount <= 0;
         else
-            -- Inicia o player
+            if ((clockCount = 0 OR clockCount = 1) AND clock = '0') then
+                clockCount <= clockCount + 1;  -- Lembrar que o valor só é atualizado no próximo ciclo
+
+                rg1: randomGenerator port map(
+                    clk => clock, 
+                    stim => pickedCard
+                );
+
+                playerSUM <= playerSUM + pickedCard;
+
+            end if;
+
+            if (playerSUM = 21) then
+                win <= '1';
+                tie <= '0';
+                lose <= '0';
+                
+                sun <= std_logic_vector(to_unsigned(playerSUM, sun'length)); -- Precisa converter para std_logic_vector
+            end if;
+            
 
 
             -- Compra duas cartas
--- randomGenerator.vhd
+++ randomGenerator.vhd
@@ -3,15 +3,14 @@ use ieee.std_logic_1164.all;
 use ieee.math_real.all;       
 use ieee.numeric_std.all;     
 
-entity random_generator is
+entity randomGenerator is
     port(
-        clk: in std_logic;                   
-        reset: in std_logic;                 
+        clk: in std_logic;                          
         stim: out integer
     );
 end random_generator;
 
-architecture behaviour of random_generator is
+architecture behaviour of randomGenerator is
 begin
     process(clk)
       variable seed1, seed2: positive;
