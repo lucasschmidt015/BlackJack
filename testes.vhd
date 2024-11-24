686d4cae84e8c1b64db1e979fe2f155a2360cddb
Lucas Schmidt <lucas.schmidt015@gmail.com>
2024-11-24 16:36:52 -0300

    Implementado controle para armazenar as cartas que já saíram.

--- testes.vhd
+++ testes.vhd
@@ -1,5 +1,89 @@
 library ieee;
 use ieee.std_logic_1164.all;
+use ieee.math_real.all;       
+use ieee.numeric_std.all;     
+
+entity randomGenerator is
+    port(
+        clk: in std_logic;  
+        ucard1: in integer;
+        ucard2: in integer;
+        ucard3: in integer;
+        ucard4: in integer;
+        ucard5: in integer;   
+        ucard6: in integer;
+        ucard7: in integer;
+        ucard8: in integer;
+        ucard9: in integer;
+        ucard10: in integer;
+        ucard11: in integer;
+        ucard12: in integer;
+        ucard13: in integer;                           
+        stim: out integer
+    );
+end randomGenerator;
+
+architecture behaviour of randomGenerator is
+begin
+    process(clk)
+      variable seed1, seed2: positive;
+      variable rand: real;  
+      variable search: std_logic:= '1';  
+      variable generatedNumber: integer range 0 to 100;                              
+    begin
+        if clk = '0' then
+            search := '1';
+            while search = '1' loop
+                uniform(seed1, seed2, rand);
+                generatedNumber := integer(trunc(rand * 14.0));
+                if generatedNumber = 1 and ucard1 < 4 then
+                    search := '0';
+                end if;
+                if generatedNumber = 2 and ucard2 < 4 then
+                    search := '0';
+                end if;
+                if generatedNumber = 3 and ucard3 < 4 then
+                    search := '0';
+                end if;
+                if generatedNumber = 4 and ucard4 < 4 then
+                    search := '0';
+                end if;
+                if generatedNumber = 5 and ucard5 < 4 then
+                    search := '0';
+                end if;
+                if generatedNumber = 6 and ucard6 < 4 then
+                    search := '0';
+                end if;
+                if generatedNumber = 7 and ucard7 < 4 then
+                    search := '0';
+                end if;
+                if generatedNumber = 8 and ucard8 < 4 then
+                    search := '0';
+                end if;
+                if generatedNumber = 9 and ucard9 < 4 then
+                    search := '0';
+                end if;
+                if generatedNumber = 10 and ucard10 < 4 then
+                    search := '0';
+                end if;
+                if generatedNumber = 11 and ucard11 < 4 then
+                    search := '0';
+                end if;
+                if generatedNumber = 12 and ucard12 < 4 then
+                    search := '0';
+                end if;
+                if generatedNumber = 13 and ucard13 < 4 then
+                    search := '0';
+                end if;
+            end loop;  
+        stim <= generatedNumber;  
+        end if;
+    end process;
+end behaviour;
+
+
+library ieee;
+use ieee.std_logic_1164.all;
 
 entity blackJack is 
     port(
@@ -64,24 +148,10 @@ begin
                 if(randonCards = '1')    then
                     rg1: randomGenerator port map(
                         clk => clock, 
-                        ucard1 => usedCard(0),
-                        ucard2 => usedCard(1),
-                        ucard3 => usedCard(2),
-                        ucard4 => usedCard(3),
-                        ucard5 => usedCard(4),   
-                        ucard6 => usedCard(5),
-                        ucard7 => usedCard(6),
-                        ucard8 => usedCard(7),
-                        ucard9 => usedCard(8),
-                        ucard10 => usedCard(9),
-                        ucard11 => usedCard(10),
-                        ucard12 => usedCard(11),
-                        ucard13: => usedCard(12),
                         stim => pickedCard
                     );
                     card <= std_logic_vector(to_unsigned(pickedCard, card'length)); -- Precisa converter para std_logic_vector
                     playerSUM := playerSUM + pickedCard;
-                    usedCard(pickedCard - 1) := usedCard(pickedCard - 1) + 1;
                 else
                     card <= std_logic_vector(to_unsigned(userCard, card'length)); -- Precisa converter para std_logic_vector
                     playerSUM := playerSUM + to_integer(unsigned(userCard));
@@ -111,24 +181,11 @@ begin
                     clockCount <= clockCount + 1;
                     rg2: randomGenerator port map(
                         clk => clock, 
-                        ucard1 => usedCard(0),
-                        ucard2 => usedCard(1),
-                        ucard3 => usedCard(2),
-                        ucard4 => usedCard(3),
-                        ucard5 => usedCard(4),   
-                        ucard6 => usedCard(5),
-                        ucard7 => usedCard(6),
-                        ucard8 => usedCard(7),
-                        ucard9 => usedCard(8),
-                        ucard10 => usedCard(9),
-                        ucard11 => usedCard(10),
-                        ucard12 => usedCard(11),
-                        ucard13: => usedCard(12),
                         stim => pickedCard
                     );
                     card <= std_logic_vector(to_unsigned(pickedCard, card'length)); -- Precisa converter para std_logic_vector
                     DealerSUM := DealerSUM + pickedCard;
-                    usedCard(pickedCard - 1) := usedCard(pickedCard - 1) + 1;
+
                 else 
                     card <= std_logic_vector(to_unsigned(userCard, card'length)); -- Precisa converter para std_logic_vector
                     DealerSUM := DealerSUM + to_integer(unsigned(userCard));
